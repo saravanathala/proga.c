0322d5e539708cba7658e760fdc296649c424ba6
saravanathala <35689043+saravanathala@users.noreply.github.com>
2018-02-07 16:07:03 +0530

    saravana6 17.c

---
+++ saravana6 17.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,c,d=0;
+printf("enter the three digit intger");
+scanf("%d",&c);
+b=a;
+while(b==0)
+{
+c=b*a;
+c=c*c;
+}
+printf("enter the digit value ");
+getch();
+return 0;
+}
