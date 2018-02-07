1f4f8ee55da6f1447260b17813a817eaafd18501
saravanathala <35689043+saravanathala@users.noreply.github.com>
2018-02-07 16:19:25 +0530

    saravana6 18.c

---
+++ saravana6 18.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int number,originalnumber,result=0;
+printf("enter the amstrong number");
+scanf("%d",&a);
+orignal number= number;
+while(original number !=0)
+{
+result +=orignal number;
+original number =10;
+}
+getch ();
+return 0;
+}
