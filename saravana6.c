04262105f857af74d7a030587fa194229a2eeaa5
saravanathala <35689043+saravanathala@users.noreply.github.com>
2018-02-07 15:51:47 +0530

    saravana6.c

---
+++ saravana6.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int i,n;
+printf("print all the even numbers");
+scanf("%d",/n);
+printf("the all even numbers");
+i=1;
+while(i<n)
+{
+if(i%2=0)
+}
+printf("all the even numbers");
+getch();
+return 0;
+}
