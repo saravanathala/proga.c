5073ba9ea684d8a74e2b177591f9e4d14d295933
saravanathala <35689043+saravanathala@users.noreply.github.com>
2018-02-19 16:02:03 +0530

    saravana19 3(2).c

---
+++ saravana19 3(2).c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main ()
+{
+int h,m,tm;
+printf("enter the hours:");
+scanf("%d",&h);
+printf("enter the minutes:");
+scanf("%d",&m);
+for(i=0;i>=1;i++);
+tm=(h*60)+m;
+printf("total time in minutes is:",tm);
+}
+getch ();
+return 0;
+}
