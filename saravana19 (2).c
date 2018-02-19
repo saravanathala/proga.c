a513f990c3d35bb462decb331feb1604e7c20aa3
saravanathala <35689043+saravanathala@users.noreply.github.com>
2018-02-19 15:43:58 +0530

    saravana19 (2).c

---
+++ saravana19 (2).c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char s;
+printf("Enter the value of s");
+scanf("%d",&s);
+if(s>=0&&s<=9)
+{
+ printf("yes");
+ }
+ else
+ {
+ printf("no");
+ }
+ getch();
+ return 0;
+ }
