808836a8017d76106271fe6cca175f647766855a
saravanathala <35689043+saravanathala@users.noreply.github.com>
2018-03-02 13:21:30 +0530

    saravana2 2(3).c

---
+++ saravana2 2(3).c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a;
+	scanf("%d",&a);
+	if(a>=1 && a<=10)
+	{
+		printf("yes");
+	}
+	else
+	{
+		printf("no");
+	}
+  getch ();
+	return 0;
+}
