4548c071f881f74bad1aa1df6dfa75a59e8739d6
saravanathala <35689043+saravanathala@users.noreply.github.com>
2018-03-02 13:46:54 +0530

    saravana2 5(3).c

---
+++ saravana2 5(3).c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int n;
+	int max,min;
+	int arr[10],a[10];
+	int i;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+	    a[i]=arr[i];
+	}
+	max=arr[0];
+	for(i=0;i<n;i++)
+	{
+  getch ();
+	return 0;
+}
