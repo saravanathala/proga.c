dce9ce22e51abe3b1b6d01f7962e29cebc058563
saravanathala <35689043+saravanathala@users.noreply.github.com>
2018-03-12 15:44:25 +0530

    saravana12 2(3).c

---
+++ saravana12 2(3).c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n,m,p;
+printf("enter the values\n");
+scanf("%d%d",&n,&m);
+p=m+n;
+if(p%2==0)
+{
+printf("yes");
+}
+else("no");
+}
+getch();
+return 0;
+}
